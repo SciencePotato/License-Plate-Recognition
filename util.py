c62440f093caf73c7fc6f2cf5cff08141e0809ef
SciencePotato <houchic@bu.edu>
2024-04-25 12:22:44 -0400

    Final version for Jason

--- util.py
+++ util.py
@@ -1,88 +1,50 @@
-#imports
+# Imports
 from ultralytics import YOLO
-from paddleocr import PaddleOCR, draw_ocr
+from paddleocr import PaddleOCR
 import easyocr
 import numpy as np
-import string
 import cv2
-from PIL import Image
-import pytesseract
-import argparse
+import pytesseract # Tried didn't succeed
 import imutils
 
 # Setup model
 reader = easyocr.Reader(['en'], gpu = False)
 yoloModel = YOLO('./models/yolov8n.pt')
 licensePlateModel = YOLO('./models/license_plate_detector.pt')
-# attempted fine-tuning
+
+# Attempted fine-tuning
 # licensePlateModel.train(data = "/Users/houchichan/Desktop/Dev/CS585/License-Plate-Recognition/data.yaml", epochs = 5, batch = 16)
-ocr = PaddleOCR(use_angle_cls = True, lang = 'en') # need to run only once to download and load model into memory
+Ocr = PaddleOCR(use_angle_cls = True, lang = 'en') # need to run only once to download and load model into memory
 
-# Mapping dictionaries for character conversion
-dict_char_to_int = {'O': '0',
-                    'I': '1',
-                    'J': '3',
-                    'A': '4',
-                    'G': '6',
-                    'S': '5'}
+# Process characters that shouldn't be allowed
+licenseMap = {"@": "0",
+              "!": "1",
+              ";": "j",
+              "\'": "",
+              "?": "2",
+              "%": "2",
+              ".": "",
+              "#": "4",
+              "_": "D",
+              "~": "",
+              "]": "T",
+              "[": "C",
+              "(": "C",
+              ")": "I",
+              "=": "-",
+              "€": "E",
+              "|": "",
+              "\"": ""}
 
-dict_int_to_char = {'0': 'O',
-                    '1': 'I',
-                    '3': 'J',
-                    '4': 'A',
-                    '6': 'G',
-                    '5': 'S'}
-
-
-def license_complies_format(text):
-    '''
-    Check if the license plate complies with the specified format
-    '''
-    if len(text) != 7:
-        return False
-
-    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
-       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
-       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
-       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
-       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
-       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
-       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
-        return True
-    else:
-        return False
-
-def format_license(text):
-    '''
-    Format the license plate to comply with the specified format
-    '''
-    license_plate_ = ''
-    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
-               2: dict_char_to_int, 3: dict_char_to_int}
-    for j in [0, 1, 2, 3, 4, 5, 6]:
-        if text[j] in mapping[j].keys():
-            license_plate_ += mapping[j][text[j]]
+def formatLicensePlate(text):
+    res = ""
+    for t in text:
+        if t in licenseMap.keys(): 
+            res += licenseMap[t]
         else:
-            license_plate_ += text[j]
+            res += t
 
-    return license_plate_
-
-
-def read_license_plate(license_plate_crop):
-    '''
-    Read the license plate from the cropped image. This is done using the EasyOCR library. (Unused version)
-    '''
-    detections = reader.readtext(license_plate_crop)
-
-    for detection in detections:
-        bbox, text, score = detection
-
-        text = text.upper().replace(' ', '')
-
-        if license_complies_format(text):
-            return format_license(text), score
-
-    return None, 0
+    return res
 
 def readLicenseImage(license_plate_crop):
     '''
@@ -91,11 +53,11 @@ def readLicenseImage(license_plate_crop):
     detections = reader.readtext(license_plate_crop)
 
     for detection in detections:
-        bbox, text, score = detection
+        _, text, score = detection
 
         text = text.upper().replace(' ', '')
 
-        return text, score
+        return formatLicensePlate(text), score
 
     return None, 0
 
@@ -103,7 +65,7 @@ def associate(licensePlate, tracks):
     '''
     Associate the license plate with the vehicle track given from deepsort
     '''
-    x1, y1, x2, y2, score, id = licensePlate
+    x1, y1, x2, y2, _, _ = licensePlate
 
     for trackObj in tracks:
         carX1, carY1, carX2, carY2 = trackObj.to_tlbr()
@@ -112,27 +74,6 @@ def associate(licensePlate, tracks):
             return carX1, carY1, carX2, carY2, carId
 
     return -1, -1, -1, -1, -1
-
-def get_car(license_plate, vehicle_track_ids):
-    '''
-    Associate the license plate with the vehicle track given from deepsort (Unused version)
-    '''
-    x1, y1, x2, y2, score, class_id = license_plate
-
-    foundIt = False
-    for j in range(len(vehicle_track_ids)):
-        xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_ids[j]
-
-        if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
-            car_indx = j
-            foundIt = True
-            break
-
-    if foundIt:
-        return vehicle_track_ids[car_indx]
-
-    return -1, -1, -1, -1, -1
-
 
 def deskew(image):
     '''
@@ -148,75 +89,53 @@ def deskew(image):
         center = (w // 2, h // 2)
         M = cv2.getRotationMatrix2D(center, angle, 1.0)
         rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC,
-        borderMode=cv2.BORDER_REPLICATE)
+        borderMode = cv2.BORDER_REPLICATE)
     return rotated
 
 def order_points(pts):
     '''
-    Order the points of the license plate
+    Transpose Coordinate to Object center and find angles of the corner in a sorted format
     '''
-    # Step 1: Find object center
     center = np.mean(pts)
-    # Step 2: Transpose Coordinate to object center
     shifted = pts - center
-    # Step #3: Find angles subtended from centroid to each corner point
     theta = np.arctan2(shifted[:, 0], shifted[:, 1])
-    # Step #4: Return vertices ordered by theta
     ind = np.argsort(theta)
     return pts[ind]
 
-def getContours(img, original):  # Change - pass the original image too
+def warpImage(img, original):  
     '''
-    Get the contours of the license plate, then warp the image such that the license plate is straight
+    Warp image by finding the biggest contour and mapping it to the original image size
     '''
     biggest = np.array([])
     maxArea = 0
-    imgContour = original.copy()  # Make a copy of the original image to return
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     index = None
-    for i, cnt in enumerate(contours):  # Change - also provide index
+    for i, cnt in enumerate(contours):  
         area = cv2.contourArea(cnt)
         peri = cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+        # To determine if the biggest Contour represents a rectangle / square
         if area > maxArea and len(approx) == 4:
             biggest = approx
             maxArea = area
-            index = i  # Also save index to contour
+            index = i  
 
-    warped = None  # Stores the warped license plate image
-    if index is not None: # Draw the biggest contour on the image
-        cv2.drawContours(imgContour, contours, index, (255, 0, 0), 3)
-
-        src = np.squeeze(biggest).astype(np.float32) # Source points
+    warped = None 
+    if index is not None: 
+        src = np.squeeze(biggest).astype(np.float32) 
         height = original.shape[0]
         width = original.shape[1]
-        # Destination points
         dst = np.float32([[0, 0], [0, height - 1], [width - 1, 0], [width - 1, height - 1]])
 
-        # Order the points correctly
         biggest = order_points(src)
         dst = order_points(dst)
 
-        # Get the perspective transform
-        M = cv2.getPerspectiveTransform(src, dst)
-
-        # Warp the image
+        # Get the perspective transform and warp image
+        M = cv2.getPerspectiveTransform(biggest, dst)
         img_shape = (width, height)
         warped = cv2.warpPerspective(original, M, img_shape, flags=cv2.INTER_LINEAR)
 
-    return biggest, imgContour, warped  # Change - also return drawn image
-
-# TRY PADDLEOCR | DOESN'T WORK
-def renderImagePaddleOCR(imagePath, imageName, outputPath):
-    '''
-    Render the image using the PaddleOCR library
-    '''
-    img_path = imagePath + "/" + imageName
-    result = ocr.ocr(img_path, cls=False, det=False)
-    for idx in range(len(result)):
-        res = result[idx]
-        for line in res:
-            print(line)
+    return warped 
 
 def renderImageEasyOCR(imagePath, imageName, outputPath):
     '''
@@ -243,20 +162,20 @@ def renderImageEasyOCR(imagePath, imageName, outputPath):
                 # img = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
                 kernel = np.ones((3,3))
                 imgGray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-                # imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1)
-                imgBlur = cv2.bilateralFilter(imgGray, 5, 75, 75)
+                imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1)
+                # imgBlur = cv2.bilateralFilter(imgGray, 5, 75, 75)
                 # imgBlur = cv2.medianBlur(imgGray, 3)
                 imgCanny = cv2.Canny(imgBlur, 0, 500, apertureSize=5)
                 imgDial = cv2.dilate(imgCanny, kernel, iterations=3)
-                imgThres = cv2.erode(imgDial, kernel, iterations=2)
-                biggest, imgContour, warped = getContours(imgThres, crop)
+                imgErode = cv2.erode(imgDial, kernel, iterations=2)
+                warped = warpImage(imgErode, crop)
 
                 # Final processing
-                _, originalThres = cv2.threshold(imgGray, 64, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                _, originalThres = cv2.threshold(imgGray, 64, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
                 grayWarped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-                _, warpedThres = cv2.threshold(grayWarped, 64, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                _, warpedThres = cv2.threshold(grayWarped, 64, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-                # Text Reading
+                # Text Reading | Based on whichever grants the best result
                 oriText, oriScore = readLicenseImage(originalThres)
                 warpText, warpScore = readLicenseImage(warpedThres)
                 text, score = warpText, warpScore
@@ -270,6 +189,8 @@ def renderImageEasyOCR(imagePath, imageName, outputPath):
                             0.6,
                             (0, 255, 0, 255),
                             2)
+
+                # Performance Counter
                 if score >= 0.8: 
                     temp80 += 1
                 elif score >= 0.5:
@@ -277,10 +198,10 @@ def renderImageEasyOCR(imagePath, imageName, outputPath):
                 else:
                     sub50 += 1
             except:
-                x1, y1, x2, y2, score, id = detection
+                x1, y1, x2, y2, score, _ = detection
                 crop = frame[int(y1): int(y2), int(x1): int(x2)]
                 imgGray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-                _, originalThres = cv2.threshold(imgGray, 64, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                _, originalThres = cv2.threshold(imgGray, 64, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
                 text, score = readLicenseImage(originalThres)
                 cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 0, 255), 4)
                 cv2.putText(frame,
@@ -290,17 +211,34 @@ def renderImageEasyOCR(imagePath, imageName, outputPath):
                             0.6,
                             (0, 255, 0, 255),
                             2)
+
+                # Performance Counter
                 if score >= 0.8: 
                     temp80 += 1
                 elif score >= 0.5:
                     temp50 += 1
                 else:
                     sub50 += 1
+        
+        print(text)
+        cv2.imshow("Final", frame)
+        cv2.waitKey(0)
     except:
         print("Error, Something went Wrong")
     return sub50, temp50, temp80
-    # cv2.imshow("Output", frame)
-    # cv2.waitKey(0)
+
+# TRY PADDLEOCR | DOESN'T WORK RAN FOR 10 + HOURS ON SINGULAR IMAGE WITHOUT ANY RESULT 
+def renderImagePaddleOCR(imagePath, imageName, outputPath):
+    '''
+    Render the image using the PaddleOCR library
+    '''
+    img_path = imagePath + "/" + imageName
+    result = Ocr.ocr(img_path, cls=False, det=False)
+    for idx in range(len(result)):
+        res = result[idx]
+        for line in res:
+            print(line)
+
 
 # TRY MORE FILTRATION / POST PROCESSING | TESSERACT KINDA BROKEN
 def renderImageTesseractOCR(imagePath, imageName, outputPath):
@@ -311,7 +249,7 @@ def renderImageTesseractOCR(imagePath, imageName, outputPath):
     detections = licensePlateModel.predict(imagePath + "/" + imageName)
     try:
         for detection in detections[0].boxes.data.tolist():
-            x1, y1, x2, y2, score, id = detection
+            x1, y1, x2, y2, _, _ = detection
             image = original[int(y1): int(y2), int(x1): int(x2)]
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
